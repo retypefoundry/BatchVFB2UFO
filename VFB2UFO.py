56a92ee3b7a04ad82bbd0e6130f718590b0080e2
retypefoundry <retypefoundry@users.noreply.github.com>
2015-01-30 20:06:35 +0100

    Create VFB2UFO.py

---
+++ VFB2UFO.py
@@ -0,0 +1,29 @@
+#Run VFB2UFO in Batch: VFB > UFO
+import os
+
+# small function to collect all files with (a) particular extension(s) in a directory
+def getFiles(root, ext):
+   files = []
+   allfiles = os.listdir(root)
+   for myfile in allfiles:
+      if os.path.splitext(myfile)[1] in ext:
+         files.append(os.path.join(root, myfile))
+   return files
+
+root = '/Users/ramiromacpro/Desktop/02-VFB2UFO'   # ! define your own directory here
+settings = "-64 "    # ! define your settings for VFB2UFO
+
+new_root = "mkdir " + root +"/UFOs"
+os.system(new_root) 
+
+if not os.path.exists(root):
+   print 'Not a valid directory'
+else:
+   ext = ['.vfb']
+   filenames = getFiles(root, ext)
+
+for n in filenames:
+	run_VBFB2UFO = "vfb2ufo "+ settings + n 
+	move_files = "mv " + root + "/*.ufo" +" "+ new_root+"/"
+	os.system(run_VBFB2UFO),os.system(move_files) 
+	
